dcbb463ca956c243c1a5eebc79b3ddbc2d1bf8b4
KY <sungpring@naver.com>
2021-04-23 10:09:58 +0900

    y

--- src/main/java/com/spboot/test/Application.java
+++ src/main/java/com/spboot/test/Application.java
@@ -1,13 +1,30 @@
 package com.spboot.test;
 
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean
+	public ServletWebServerFactory swsFactoryInit() {
+		TomcatServletWebServerFactory swsFactory = new TomcatServletWebServerFactory();
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setScheme("http");
+		connector.setSecure(false);
+		connector.setPort(80);
+		connector.setRedirectPort(443);
+		swsFactory.addAdditionalTomcatConnectors(connector);
+		return swsFactory;
 	}
 
 }
